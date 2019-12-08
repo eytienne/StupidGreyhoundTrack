9c9b263953ce523f8f001eccf8b2efe00c0dce83
eytienne <arabeyre.etienne@gmail.com>
2019-12-09 00:23:09 +0100

    tkinter menu layout

--- Menu.py
+++ Menu.py
@@ -1,3 +1,4 @@
+from tkinter import messagebox
 from tkinter import *
 from Game import Game
 from threading import Thread
@@ -8,12 +9,23 @@ class Menu(Tk):
     def __init__(self):
         super().__init__(className=Game.WINDOW_TITLE)
 
-        playBTN = Button(self, text="Play", command=self.run_game)
-        playBTN.grid(row=0, column=0)
+        homeFrame = Frame(self)
+        buttonsFrame = Frame(homeFrame)
 
-        self.columnconfigure(0, weight=1)
+        playBTN = Button(buttonsFrame, text="Play", command=self.run_game, bg="red")
+        playBTN.pack(fill="x")
+        settingsBTN = Button(buttonsFrame, text="Settings", command=self.open_settings, bg="blue", fg="white")
+        settingsBTN.pack(fill="x")
+        
+        buttonsFrame.grid()
+        
+        homeFrame.rowconfigure(0, weight=1)
+        homeFrame.columnconfigure(0, weight=1)
+        homeFrame.grid(row=0, column=0, sticky="nesw")
+
         self.rowconfigure(0, weight=1)
-
+        self.columnconfigure(0, weight=1)
+        
         w, h = self.winfo_screenwidth(), self.winfo_screenheight()
         self.geometry("%dx%d+0+0" % (w, h))
 
@@ -23,6 +35,9 @@ class Menu(Tk):
         gameThread.start()
         gameThread.join()
         self.state('normal')
+
+    def open_settings(self):
+        messagebox.showinfo(message="Coming soon...")
 
     pass
 
